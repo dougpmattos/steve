21e0b0104f0bafceb28a959dd9e57a8f03d659ec
brunobolandini <bruno.svp@gmail.com>
2017-02-08 15:35:36 -0200

    Limitador de 1 char adicionado ao campo de texto

--- src/view/stevePane/PreferencesWindow.java
+++ src/view/stevePane/PreferencesWindow.java
@@ -9,6 +9,8 @@ import view.temporalViewPane.TemporalViewPane;
 import controller.Controller;
 import model.common.Media;
 import model.temporalView.enums.InteractivityKeyType;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
@@ -157,6 +159,11 @@ public class PreferencesWindow extends Stage {
         blueKeyField.setId("blue-key-field");
         yellowKeyField.setId("yellow-key-field");
         
+        addTextLimiter(redKeyField, 1);
+        addTextLimiter(greenKeyField, 1);
+        addTextLimiter(blueKeyField, 1);
+        addTextLimiter(yellowKeyField, 1);
+        
         HBox interactivityKeySeparator = new HBox();
         interactivityKeySeparator.setId("separator");
         VBox interactivityKeySubtitleSeparatorContainer = new VBox();
@@ -180,105 +187,23 @@ public class PreferencesWindow extends Stage {
         formGridPane.add(blueKeyField, 1, 5);
         formGridPane.add(interactivityKeyYellowLabel, 0, 6);
         formGridPane.add(yellowKeyField, 1, 6);
-        
-    	
-		
-		
-        //Fill stage with content
-		//StackPane root = new StackPane();
-        
-        //Scene scene = new Scene(rootGroup, 250, 320); //width x height
-        //scene.getStylesheets().add("view/common/styles/preferencesWindow.css");
-		//stage.setTitle(Language.translate("preferences"));
-		//root.setId("pane");
-		//rootGroup.setId("pane");        
-       
-        //final Text title = new Text(25, 25, "Controle de teclas");
-        //title.setFill(Color.BLACK);
-        //title.setId("pane");
-        //rootGroup.getChildren().add(title);
-                
-        
-        /*
-        final Text larr = new Text(25,50,"←");
-        larr.setFill(Color.BLACK);
-        rootGroup.getChildren().add(larr);
-        
-        final Text input_larr = new Text(100,50,"◄");
-        input_larr.setFill(Color.BLACK);
-        rootGroup.getChildren().add(input_larr);
-        
-        final Text rarr = new Text(25, 75, "→");
-        larr.setFill(Color.BLACK);
-        rootGroup.getChildren().add(rarr);
-        
-        final Text input_rarr = new Text(100,75,"►");
-        input_rarr.setFill(Color.BLACK);
-        rootGroup.getChildren().add(input_rarr);
-        
-        final Text darr = new Text(25, 100, "↓");
-        darr.setFill(Color.BLACK);
-        rootGroup.getChildren().add(darr);
-        
-        final Text input_darr = new Text(100, 100, "▼");
-        input_darr.setFill(Color.BLACK);
-        rootGroup.getChildren().add(input_darr);
-        
-        final Text uarr = new Text(25, 125, "↑");
-        uarr.setFill(Color.BLACK);
-        rootGroup.getChildren().add(uarr);
-        
-        final Text input_uarr = new Text(100, 125, "▲");
-        input_uarr.setFill(Color.BLACK);
-        rootGroup.getChildren().add(input_uarr);
-        
-        final Text enter = new Text(25, 150, "ENTER");
-        enter.setFill(Color.BLACK);
-        rootGroup.getChildren().add(enter);
-        
-        final Text input_enter = new Text(100, 150, "ENTER");
-        input_enter.setFill(Color.BLACK);
-        rootGroup.getChildren().add(input_enter);//
-        
-        final Text red = new Text(25, 175, "RED");
-        red.setFill(Color.RED);
-        rootGroup.getChildren().add(red);
-        
-        final Text input_red = new Text(100, 175, "0");
-        input_red.setFill(Color.RED);
-        rootGroup.getChildren().add(input_red);
-        
-        final Text green = new Text(25, 200, "GREEN");
-        green.setFill(Color.GREEN);
-        rootGroup.getChildren().add(green);
-        
-        final Text input_green = new Text(100, 200, "1");
-        input_green.setFill(Color.GREEN);
-        rootGroup.getChildren().add(input_green);
-        
-        final Text blue = new Text(25, 225, "BLUE");
-        blue.setFill(Color.BLUE);
-        rootGroup.getChildren().add(blue);
-        
-        final Text input_blue = new Text(100, 225, "2");
-        input_blue.setFill(Color.BLUE);
-        rootGroup.getChildren().add(input_blue);
-        
-        final Text yellow = new Text(25, 250, "YELLOW");
-        yellow.setFill(Color.YELLOW);
-        rootGroup.getChildren().add(yellow);
-        
-        final Text input_yellow = new Text(100, 250, "3");
-        input_yellow.setFill(Color.YELLOW);
-        rootGroup.getChildren().add(input_yellow);
-        
-        */
-        //formGridPane.add(rootGroup, 0, 1);
-        //text1.setFont(Font.font(java.awt.Font.SERIF, 18));
 
         
         return formGridPane;
 
     	
     }
+    
+    public static void addTextLimiter(final TextField tf, final int maxLength) {
+        tf.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+                if (tf.getText().length() > maxLength) {
+                    String s = tf.getText().substring(0, maxLength);
+                    tf.setText(s);
+                }
+            }
+        });
+    }
+
 }
